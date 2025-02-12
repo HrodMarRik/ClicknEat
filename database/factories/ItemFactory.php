48d525d4bc05dd0811b9fe32823cd7606bbacadb
HrodMarRik <romaric.chanavat@gmail.com>
2025-02-12 14:13:10 +0100

    auto-up

--- database/factories/ItemFactory.php
+++ database/factories/ItemFactory.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Items>
  */
-class ItemsFactory extends Factory
+class ItemFactory extends Factory
 {
     /**
      * Define the model's default state.
